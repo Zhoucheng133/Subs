438e154a7e034c52c046ec8f6a938098a7a705fe
zhoucheng <zhoucheng133@outlook.com>
2024-01-30 11:27:53 +0800

    切换使用Fluent UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 // ignore_for_file: sized_box_for_whitespace, prefer_const_constructors, prefer_const_literals_to_create_immutables
 
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 import 'package:bitsdojo_window/bitsdojo_window.dart';
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:get/get.dart';
+import 'package:subs/paras/paras.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
   
   doWhenWindowReady(() {
     const initialSize = Size(800, 600);
     appWindow.minSize = initialSize;
     appWindow.size = initialSize;
+    appWindow.maxSize=initialSize;
     appWindow.alignment = Alignment.center;
     appWindow.show();
   });
@@ -24,13 +28,14 @@ class MainApp extends StatefulWidget {
 
 class _MainAppState extends State<MainApp> {
 
-  bool checked = false;
+  final Controller c=Get.put(Controller());
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Stack(
+    return FluentApp(
+      home: Container(
+        color: Colors.white,
+        child: Stack(
           children: [
             Positioned(
               top: 0,
@@ -45,11 +50,49 @@ class _MainAppState extends State<MainApp> {
             ),
             Positioned(
               top: 30,
-              child: Container()
+              child: Container(
+                // color: Colors.red,
+                width: MediaQuery.of(context).size.width,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 30, right: 30, top: 30),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Row(
+                              children: [
+                                Button(
+                                  child: Text("选择视频目录"), 
+                                  onPressed: (){
+                                    print(c.videoDir.value.text);
+                                  }
+                                ),
+                                SizedBox(width: 10,),
+                                Expanded(
+                                  child: Obx(() => 
+                                    TextBox(
+                                      controller: c.videoDir.value,
+                                    ),
+                                  )
+                                )
+                              ],
+                            )
+                          ),
+                          SizedBox(width: 10,),
+                          Expanded(
+                            child: Container()
+                          )
+                        ],
+                      )
+                    ],
+                  )
+                )
+              )
             )
           ],
-        )
-      ),
+        ),
+      )
     );
   }
 }
--- lib/paras/paras.dart
+++ lib/paras/paras.dart
@@ -1,5 +1,5 @@
+import 'package:fluent_ui/fluent_ui.dart';
 import 'package:get/get.dart';
 class Controller extends GetxController{
-  var step=0.obs;
-  
+  var videoDir=TextEditingController().obs;
 }
