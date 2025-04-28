45d43f554688477a882d03a575df6069594a3db5
zhoucheng <zhoucheng133@outlook.com>
2025-04-28 19:13:36 +0800

    隐藏在macOS系统下的窗口控件

--- lib/main_window.dart
+++ lib/main_window.dart
@@ -222,134 +222,139 @@ class _MainWindowState extends State<MainWindow> with WindowListener {
             child: Row(
               children: [
                 Expanded(child: DragToMoveArea(child: Container(),)),
-                GestureDetector(
-                  onTap: (){
-                    showDialog(
-                      context: context, 
-                      builder: (context)=>ContentDialog(
-                        title: Text('关于Subs', style: GoogleFonts.notoSansSc(),),
-                        content: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Align(
-                              alignment: Alignment.center,
-                              child: SizedBox(
-                                width: 100,
-                                height: 100,
-                                child: Image.asset('assets/icon.png')
-                              ),
-                            ),
-                            Text(
-                              'Subs', 
-                              style: GoogleFonts.notoSansSc(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 20
-                              ),
-                            ),
-                            const SizedBox(height: 10,),
-                            Text(
-                              v.version,
-                              style: GoogleFonts.notoSansSc(
-                                color: Colors.grey[80],
-                              ),
-                            ),
-                            const SizedBox(height: 15,),
-                            GestureDetector(
-                              onTap: () async {
-                                final Uri url = Uri.parse('https://github.com/Zhoucheng133/Subs');
-                                await launchUrl(url);
-                              },
-                              child: MouseRegion(
-                                cursor: SystemMouseCursors.click,
-                                child: Row(
-                                  mainAxisSize: MainAxisSize.min,
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    const FaIcon(
-                                      FontAwesomeIcons.github,
-                                      size: 15,
-                                    ),
-                                    const SizedBox(width: 5,),
-                                    Text(
-                                      '本项目地址',
-                                      style:  GoogleFonts.notoSansSc(
-                                
-                                      ),
-                                    )
-                                  ],
+                Platform.isWindows ? Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    GestureDetector(
+                      onTap: (){
+                        showDialog(
+                          context: context, 
+                          builder: (context)=>ContentDialog(
+                            title: Text('关于Subs', style: GoogleFonts.notoSansSc(),),
+                            content: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Align(
+                                  alignment: Alignment.center,
+                                  child: SizedBox(
+                                    width: 100,
+                                    height: 100,
+                                    child: Image.asset('assets/icon.png')
+                                  ),
                                 ),
-                              ),
-                            ),
-                            const SizedBox(height: 10),
-                            GestureDetector(
-                              onTap: ()=>showLicensePage(context: context),
-                              child: MouseRegion(
-                                cursor: SystemMouseCursors.click,
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: [
-                                    const FaIcon(
-                                      FontAwesomeIcons.certificate,
-                                      size: 15,
-                                    ),
-                                    const SizedBox(width: 5,),
-                                    Padding(
-                                      padding: const EdgeInsets.only(bottom: 2),
-                                      child: Text(
-                                        '许可证',
-                                        style: GoogleFonts.notoSansSc(
-                                          fontSize: 13,
+                                Text(
+                                  'Subs', 
+                                  style: GoogleFonts.notoSansSc(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20
+                                  ),
+                                ),
+                                const SizedBox(height: 10,),
+                                Text(
+                                  v.version,
+                                  style: GoogleFonts.notoSansSc(
+                                    color: Colors.grey[80],
+                                  ),
+                                ),
+                                const SizedBox(height: 15,),
+                                GestureDetector(
+                                  onTap: () async {
+                                    final Uri url = Uri.parse('https://github.com/Zhoucheng133/Subs');
+                                    await launchUrl(url);
+                                  },
+                                  child: MouseRegion(
+                                    cursor: SystemMouseCursors.click,
+                                    child: Row(
+                                      mainAxisSize: MainAxisSize.min,
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      children: [
+                                        const FaIcon(
+                                          FontAwesomeIcons.github,
+                                          size: 15,
                                         ),
-                                      ),
-                                    )
-                                  ],
+                                        const SizedBox(width: 5,),
+                                        Text(
+                                          '本项目地址',
+                                          style:  GoogleFonts.notoSansSc(
+                                    
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  ),
                                 ),
-                              ),
-                            )
-                          ],
-                        ),
-                        actions: [
-                          FilledButton(
-                            child: Text('好的', style: GoogleFonts.notoSansSc(),), 
-                            onPressed: (){
-                              Navigator.pop(context);
-                            }
+                                const SizedBox(height: 10),
+                                GestureDetector(
+                                  onTap: ()=>showLicensePage(context: context),
+                                  child: MouseRegion(
+                                    cursor: SystemMouseCursors.click,
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      crossAxisAlignment: CrossAxisAlignment.center,
+                                      children: [
+                                        const FaIcon(
+                                          FontAwesomeIcons.certificate,
+                                          size: 15,
+                                        ),
+                                        const SizedBox(width: 5,),
+                                        Padding(
+                                          padding: const EdgeInsets.only(bottom: 2),
+                                          child: Text(
+                                            '许可证',
+                                            style: GoogleFonts.notoSansSc(
+                                              fontSize: 13,
+                                            ),
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                            actions: [
+                              FilledButton(
+                                child: Text('好的', style: GoogleFonts.notoSansSc(),), 
+                                onPressed: (){
+                                  Navigator.pop(context);
+                                }
+                              )
+                            ],
                           )
-                        ],
-                      )
-                    );
-                  },
-                  child: Container(
-                    color: hoverAbout ? Colors.grey[20] : Colors.white,
-                    width: 50,
-                    child: MouseRegion(
-                      onEnter: (_){
-                        setState(() {
-                          hoverAbout=true;
-                        });
+                        );
                       },
-                      onExit: (_){
-                        setState(() {
-                          hoverAbout=false;
-                        });
-                      },
-                      child: const Center(
-                        child: FaIcon(
-                          FontAwesomeIcons.circleInfo,
-                          size: 14,
+                      child: Container(
+                        color: hoverAbout ? Colors.grey[20] : Colors.white,
+                        width: 50,
+                        child: MouseRegion(
+                          onEnter: (_){
+                            setState(() {
+                              hoverAbout=true;
+                            });
+                          },
+                          onExit: (_){
+                            setState(() {
+                              hoverAbout=false;
+                            });
+                          },
+                          child: const Center(
+                            child: FaIcon(
+                              FontAwesomeIcons.circleInfo,
+                              size: 14,
+                            ),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ),
-                WindowCaptionButton.minimize(
-                  onPressed: ()=>minimize(),
-                ),
-                WindowCaptionButton.close(
-                  onPressed: ()=>close(),
-                )
+                    WindowCaptionButton.minimize(
+                      onPressed: ()=>minimize(),
+                    ),
+                    WindowCaptionButton.close(
+                      onPressed: ()=>close(),
+                    ),
+                  ],
+                ) : Container()
               ],
             ),
           ),
